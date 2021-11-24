f64a2ef21434f0203100fde44e18a4ab75da89bd
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-11-24 22:20:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activSlide = 0) {
+	const slides = document.querySelectorAll('.slide')
+	slides[activSlide].classList.add('active')
 
-for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClasses()
-		slide.classList.add('active')
-	})
-}
-
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('active')
+	for (const slide of slides) {
+		slide.addEventListener('click', () => {
+			clearActiveClasses()
+			slide.classList.add('active')
+		})
 	}
-	)
+
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('active')
+		}
+		)
+	}
 }
+
+slidesPlugin(4);
